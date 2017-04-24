6e154638ba8c1bb150d50f66ab16fc3c9a583f89
kfjustis <kfjustis@gmail.com>
2017-04-23 22:45:18 -0500

    Successfully validates commands

--- client.py
+++ client.py
@@ -1,7 +1,32 @@
 import socket
 import sys
 
+def wait_for_server(error_msg, connection):
+    waiting = True
+    while waiting == True:
+        response = connection.recv(1024).decode()
+        if response == "ack":
+            waiting = False
+        elif response == "error":
+            print(error_msg)
+            print("here")
+        elif response == "invalid_command":
+            waiting = False
+            print(error_msg)
+            message = input("\nEnter command: ")
+            connection.send(message.encode())
+            return "invalid command"
+        else:
+            print("Error was: " + error_msg)
+            print("Waiting for server response...")
+
+    return True
+
 def Main():
+    # vars
+    ack = "ack"
+    error = "error"
+
     # connect and ask for username
     host = socket.gethostname()
     port = 12450
@@ -9,9 +34,23 @@ def Main():
     localSocket.connect((host, port))
     print()
     print("Connected to " + str(host) + '/' + str(port) + '!')
-    message = input("\nUsername: ")
+
+    # ask for command
+    message = input("\nEnter command: ")
+
+    # handle sending command
     localSocket.send(message.encode())
 
+    # wait for valid response
+    while wait_for_server("\nCommand invalid! If you haven't logged in, please do so.",
+        localSocket) == "invalid command":
+        print("loop")
+        pass
+
+    message = input("\nUsername: ")
+    localSocket.send(message.encode())
+    wait_for_server("\nInvalid username! Terminating client...", localSocket)
+    '''
     waiting = True
     while waiting is True:
         response = localSocket.recv(1024).decode()
@@ -23,8 +62,10 @@ def Main():
             sys.exit()
         else:
             print("Waiting for server response...")
+    '''
 
     print("\nUsername matched by server!")
+
     '''
     # connect and ask for password
     localSocket = socket.socket()
--- server.py
+++ server.py
@@ -1,17 +1,27 @@
 import socket
 import sys
 
-def print_error_and_exit(e_msg, connection):
-    print(e_msg)
+def print_error_and_send(error_msg, connection):
+    print(error_msg)
     error = "error"
     connection.send(error.encode())
-    connection.close()
-    sys.exit()
+
+def check_commands(input_command, comm_list):
+    for comm in comm_list:
+        if input_command == comm:
+            return True
+
+    return False
 
 def Main():
     # vars
     ack = "ack"
     error = "error"
+    invalid_command = "invalid_command"
+    running = True
+    login = False
+    commandFailing = False
+    commandList = ["send", "login", "logout"]
 
     # read users from file
     file = open("accounts.txt", "r")
@@ -23,16 +33,45 @@ def Main():
     localSocket = socket.socket()
     localSocket.bind((host, port))
     localSocket.listen(5)
-
-    # read username from client and validate
     conn, addr = localSocket.accept()
-    print("\nConnection from: " + str(addr))
     if conn:
         running = True
-    while running is not False:
+
+    # handle reading command (MUST WAIT FOR VALID COMMAND)
+    while running != False:
+        command = conn.recv(1024).decode()
+        if not command:
+            print_error_and_send("Invalid data! Terminating server...", conn)
+            connection.close()
+            sys.exit()
+
+        validCommand = check_commands(command, commandList)
+        while validCommand == False and login == False:
+            if command == "login":
+                conn.send(ack.encode())
+                print("\nValidating login...")
+                login = True
+            else:
+                print("Command denied. Please login first.")
+                conn.send(invalid_command.encode())
+                command = conn.recv(1024).decode()
+                if not command:
+                    print_error_and_send("Invalid data! Terminating server...", conn)
+                    # we don't want to close the connection and exit cause this
+                    # should loop until the command is valid
+
+        running = False
+
+    # read username from client and validate
+    print("\nConnection from: " + str(addr))
+    running = True
+    while running != False:
+        conn.send(ack.encode())
         username = conn.recv(1024).decode()
         if not username:
-            print_error_and_exit("Invalid data! Terminating server...", conn)
+            print_error_and_send("Invalid data! Terminating server...", conn)
+            conn.close()
+            sys.exit()
 
         validUname = False
         for name in userInfo:
@@ -40,7 +79,9 @@ def Main():
                 validUname = True
 
         if (validUname == False):
-            print_error_and_exit("Username not found! Terminating server...", conn)
+            print_error_and_send("Username not found! Terminating server...", conn)
+            conn.close()
+            sys.exit()
 
         print("\nUsername found!")
 
@@ -55,7 +96,7 @@ def Main():
     while running is not False:
         password = conn.recv(1024).decode()
         if not password:
-            print_error_and_exit("Invalid data! Terminating server...", conn)
+            print_error_and_send("Invalid data! Terminating server...", conn)
 
         validPword = False
         for name in userInfo:
@@ -63,7 +104,7 @@ def Main():
                 validPword = True
 
         if (validUname == False):
-            print_error_and_exit("Password not found! Terminating server...", conn)
+            print_error_and_send("Password not found! Terminating server...", conn)
 
         print("Password found!")
 
